a5fb6ab51f51c7f1123f01a8c67efbf6060aa935
алкаш <all-kas@yandex.ru>
2019-10-04 19:54:09 +0300

    commit

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from 'readline-sync';
-
-
-export const user = () => {
-  const userName = readLineSync.question('May I have your name? ');
-  console.log(`Hi, ${userName}!\n`);
-  return userName;
-};
