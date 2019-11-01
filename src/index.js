1d205e9839b116137b859ecae70cd51ed78916e3
алкаш <all-kas@yandex.ru>
2019-11-01 21:37:54 +0300

    123

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import readlineSync from 'readline-sync';
 
+const welcome = 'Welcome to the Brain Games!';
+const text = 'Answer "yes" if the number is even, otherwise answer "no".';
+const numRandom = (min, max) => Math.floor(Math.random() * (max - min));
+
 const user = () => {
-  console.log('Welcome to the Brain Games!');
+  console.log(welcome);
+  console.log(text);
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hi ${userName}!`);
-  return userName;
+  console.log(`Hello ${userName}!`);
 };
 export default user;
+export const number = numRandom(1, 99);
+export const compare = () => {
+  if (number % 2 === 0) {
+    return true;
+  }
+  return false;
+};
+export const esEven = (num) => {
+  const even = compare(num) ? 'yes' : 'no';
+};
+export const evenOrNot = () => {
+  readlineSync.question('Your answer: ');
+};
