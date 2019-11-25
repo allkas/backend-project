a971e0e8cd9c543f01df13d0a5cd8b31ad848dfc
алкаш <all-kas@yandex.ru>
2019-11-25 15:56:50 +0300

    6 step

---
+++ src/games/calc.js
@@ -0,0 +1,17 @@
+import game from '..';
+
+export default () => {
+  const text = 'What is the result of the expression?';
+
+  const getDataGame = () => {
+    const operation = ['+', '-', '*'];
+    const randomNum1 = Math.floor(Math.random() * Math.floor(25));
+    const randomNum2 = Math.floor(Math.random() * Math.floor(15));
+    const operNum = Math.floor(Math.random() * 3);
+    const expression = `${randomNum1} ${operation[operNum]} ${randomNum2}`;
+    const correctAnswer = parseInt(expression);
+    return [expression, correctAnswer];
+  };
+
+  game(text, getDataGame);
+};
