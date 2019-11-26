a3a38eeb2fc511961a9a1edd118c51fb13e62839
алкаш <all-kas@yandex.ru>
2019-11-26 11:02:10 +0300

    6 step

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ brain-games
 [![Build Status](https://travis-ci.com/allkas/backend-project-lvl1.svg?branch=master)](https://travis-ci.com/allkas/backend-project-lvl1)
 
 [![asciicast](https://asciinema.org/a/IWDqM6T0zLdrerPgqJONdyHcg.svg)](https://asciinema.org/a/IWDqM6T0zLdrerPgqJONdyHcg)
+[![asciicast](https://asciinema.org/a/eTUOkSQuMmB1T9AcByGxSVYz2.svg)](https://asciinema.org/a/eTUOkSQuMmB1T9AcByGxSVYz2)
--- src/games/calc.js
+++ src/games/calc.js
@@ -9,7 +9,18 @@ export default () => {
     const randomNum2 = Math.floor(Math.random() * Math.floor(15));
     const operNum = Math.floor(Math.random() * 3);
     const expression = `${randomNum1} ${operation[operNum]} ${randomNum2}`;
-    const correctAnswer = parseInt(expression, 10);
+    const answer = (num1, num2, oper) => {
+      if (String(oper) === '+') {
+        return (randomNum1 + randomNum2);
+      }
+      if (String(oper) === '-') {
+        return (randomNum1 - randomNum2);
+      }
+      if (String(oper) === '*') {
+        return (randomNum1 * randomNum2);
+      }
+    };
+    const correctAnswer = String(answer(randomNum1, randomNum2, operation[operNum]));
     return [expression, correctAnswer];
   };
 
