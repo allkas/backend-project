e5f3d0b6f6bae6d9f9afd6013996973b256e6633
алкаш <all-kas@yandex.ru>
2019-12-09 15:16:32 +0300

    fix

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,22 +4,21 @@ import numberRandom from '../utils';
 
 const textDescription = 'What is the result of the expression?';
 const operations = ['+', '-', '*'];
-
+const operand1 = numberRandom(2, 10);
+const operand2 = numberRandom(2, 10);
+const selectOperation = (num1, num2, operation) => {
+  switch (operation) {
+    case '+':
+      return operand1 + operand2;
+    case '-':
+      return operand1 - operand2;
+    case '*':
+      return operand1 * operand2;
+    default: return null;
+  }
+};
 const getDataGame = () => {
-  const operand1 = numberRandom(2, 10);
-  const operand2 = numberRandom(2, 10);
   const operationsNumbers = numberRandom(1, operations.length - 1);
-  const selectOperation = (num1, num2, operation) => {
-    switch (operation) {
-      case '+':
-        return operand1 + operand2;
-      case '-':
-        return operand1 - operand2;
-      case '*':
-        return operand1 * operand2;
-      default: return null;
-    }
-  };
   const question = `${operand1} ${operations[operationsNumbers]} ${operand2}`;
   const correctAnswer = String(selectOperation(operand1, operand2, operations[operationsNumbers]));
   return cons(question, correctAnswer);
--- src/games/progression.js
+++ src/games/progression.js
@@ -3,19 +3,20 @@ import game from '..';
 import numberRandom from '../utils';
 
 const textDescription = 'What number is missing in the progression?';
+const progressionLength = 9;
+const arrayProgression = [];
+
+const getProgression = (num1, num2) => {
+  for (let i = 0; i < progressionLength; i += 1) {
+    arrayProgression.push(num1 + num2 * i);
+  }
+  return arrayProgression;
+};
 
 const getDataGame = () => {
-  const arrayProgression = [];
-  const progressionLength = 9;
   const numberQuestion = numberRandom(1, 9);
   const startNumber = numberRandom(1, 17);
   const step = numberRandom(2, 5);
-  const getProgression = (num1, num2) => {
-    for (let i = 0; i < progressionLength; i += 1) {
-      arrayProgression.push(num1 + num2 * i);
-    }
-    return arrayProgression;
-  };
   getProgression(startNumber, step);
   const correctAnswer = String(arrayProgression[numberQuestion]);
   arrayProgression[numberQuestion] = '..';
